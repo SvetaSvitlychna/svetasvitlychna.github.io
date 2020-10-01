7f322c4d1f00ab33e8add99e90286c8b3983d6c7
SvetaSvitlychna <manulwild@gmail.com>
2020-10-01 20:17:38 +0300

    total amount was fixed

--- js/app.js
+++ js/app.js
@@ -1,168 +1,269 @@
-
-const toggleBtn = document.querySelector(".cart-toggle");
-const closeBtn = document.querySelector(".close-btn");
-const sidebar =document.querySelector(".sidebar");
+'use strict';
 
 
-function addProductToCart(content,item)
-{
-    content.querySelector('.cart-item').setAttribute('id',item.getAttribute('data-id'));
-    content.querySelector('.product-name').textContent =item.querySelector(".product-name").textContent;
-    content.querySelector('.product-price').textContent = item.querySelector(".product-price").textContent;
-    content.querySelector('.product-price').setAttribute ('data-price', item.querySelector(".product-price").textContent);
-    content.querySelector('.product-img img').setAttribute('src', item.querySelector(".product-img").getAttribute('src'));
-    return content;
+
+
+const overlayGroup = 
+[
+    {
+        liClass:'list-inline-item m-0 p-0 like-this', 
+        aClass:'btn', 
+        icon:'fas fa-thumbs-up',
+        urlClass:'',
+        capture:''
+    },
+    {
+        liClass:'list-inline-item m-0 p-0 add-to-cart', 
+        aClass:'btn', 
+        icon:'fas  fa-cart-arrow-down',
+        urlClass:'',
+        capture:''
+    },
+    {
+        liClass:'list-inline-item m-0 p-0 view-this', 
+        aClass:'btn', 
+        icon:'fas fa-info-circle',
+        capture:''
+    },
+];
+const socialGroup = [
+    {liClass:'footer-socials social-icon', 
+    aClass:'footer-link twitter', 
+    icon:'fab fa-twitter',
+    capture:'Twitter'
+},
+{liClass:'footer-socials social-icon', 
+aClass:'footer-link facebook', 
+icon:'fab fa-facebook',
+capture:'Facebook'
+},
+{liClass:'footer-socials social-icon', 
+aClass:'footer-link instagram', 
+icon:'fab fa-instagram',
+capture:'Instagram'
+},
+{liClass:'footer-socials social-icon', 
+aClass:'footer-link google', 
+icon:'fab fa-google-plus',
+capture:'Google'
+},
+];
+
+class App{
+ cart =[];
+ clearCart = document.querySelector(".clear-cart");
+ cartItems = document.querySelector(".cart-items");
+
+    constructor(){
+    const toggleBtn = document.querySelector(".cart-toggle");
+    const closeBtn = document.querySelector(".close-btn");
+    const sidebar =document.querySelector(".sidebar");
+   
+    toggleBtn.addEventListener('click',  ()=> sidebar.classList.toggle('show-sidebar'));
+    closeBtn.addEventListener("click",  () => sidebar.classList.remove("show-sidebar"));
+      this.navbarToggle();
+      this.makeShowCase();
+      document.querySelector('footer div.footerIcon').firstElementChild.innerHTML = this.makeLiGroup(socialGroup, 'navbar-footer footer-socials social-icon', '<h6 class="text-dark text-muted">Social media</h6>');
+}
+
+navbarToggle(){
+    const navToggle = document.querySelector(".nav-toggle");
+    const linksContainer = document.querySelector(".linkscont");
+    const links= document.querySelector(".links-container");
+
+    navToggle.addEventListener('click', function (){
+        const linksHeight =links.getBoundingClientRect().height;
+        const containerHeight =linksContainer.getBoundingClientRect().height;
+        if(containerHeight === 0){
+            linksContainer.style.height = `${linksHeight}px`;
+        } else{
+            linksContainer.style.height =0;
+        }
+    });
+}  
+makeLiGroup = (group,ulClass, header='')=>{
+    let lis='';
+    group.forEach(function(item){
+        lis+=`<li class ="${item.liClass}">
+        <a class = "${item.aClass}" href="#">
+        <i class = "${item.icon}"></i>${item.capture}
+        </a>
+        </li>`;
+    });
+    return `
+    ${header}
+    <ul class ="${ulClass}">
+    ${lis}
+    </ul>`;
+}
+
+makeShowCase(){
+    let result = '';
+newproduct.forEach(item=> {
+  
+    result+=this.createProductMarkUp(item);
+});
+document.querySelector('.showcase').innerHTML = result;
+}
+createProductMarkUp(data){
+    return`
+    <div  class="col-md-3 col-xl-3 col-lg-4 col-sm-6 cartItem">
+       <div class="newproduct text-center" data-id="${data.id}">
+          <div class="position-relative mb-3">
+            <a href="detail.html" class="d-block">
+             <img src="${data.image}" alt="${data.name}" class="img-fluid w-100 product-img">
+            </a>
+             <div class="product-overlay">
+             ${this.makeLiGroup(overlayGroup,'mb-0 list-inline')}
+             </div>
+           </div>
+          <h3><a href="detail.html" class="reset-anchor product-name">${data.name}</a></h3>
+         <p class="small text-muted product-price" data-price=${data.price}>${data.price}</p>
+         </div>
+    </div> `;
+}
+getProduct = (id)=>
+     newproduct.find(product =>product.id === +(id));
+
+addToCart(){
+    const addToCartButtons = [...document.querySelectorAll(".add-to-cart")];
+        addToCartButtons.forEach(button=> {
+           button.addEventListener('click',event=>{
+       let cartItem = {...this.getProduct(event.target.closest('.newproduct').getAttribute('data-id')),amount:1};
+        this.cart = [...this.cart, cartItem];
+        this.addCartItem(cartItem);
+        this.setCartTotal(this.cart);
+    });
+           });
+}
+addCartItem(item){
+    const div = document.createElement("div");
+    div.classList.add("cart-item");
+    div.setAttribute('id',item.id);
+    div.innerHTML = `
+        <div class="picture product-img">
+                <img src="${item.image}" alt="${item.name}" class="img-fluid w-100">
+            </div>
+            <div class="product-name p-auto">${item.name}</div>
+            <div class="remove-btn text-right">
+                <a class="reset-anchor m-auto" href="#">
+                    <i class="fas fa-trash-alt" data-id=${item.id}></i>
+                </a>
+            </div>
+            <div class="quantity">
+                <div class="border d-flex justify-content-around mx-auto">
+                    <i class="fas fa-caret-left inc-dec-btn" data-id=${item.id}></i>
+                    <span class="border-1 p-1 amount">${item.amount}</span>
+                    <i class="fas fa-caret-right inc-dec-btn" data-id=${item.id}></i>
+                </div>
+            </div>
+            <div class="price">
+                $<span class="product-price itemprice">${item.price}</span>
+            </div>`;
+ this.cartItems.appendChild(div);
+}
+filterItem = (cart, curentItem) => cart.filter(item => item.id !== +(curentItem.dataset.id));
+
+findItem = (cart, curentItem) => cart.find(item => item.id === +(curentItem.dataset.id));
+
+clear = () =>{
+    this.cart =[];
+    while (this.cartItems.children.length>0){
+        this.cartItems.removeChild(this.cartItems.children[0]);
+    }
+    this.setCartTotal(this.cart)
+}
+
+setCartTotal(cart){
+     let temptTotal = 0;
+     let itemTotal =0;
+        cart.map(item => {
+         temptTotal+= item.price * item.amount;
+                itemTotal += item.amount;
+     });
+     document.querySelector('.cart-total').textContent = parseFloat(temptTotal.toFixed(2));
+     document.querySelector('.count-items').textContent = itemTotal;
+    
     
 }
 
-function createMarkup(data){
- return `
- <div class="col-md-3 col-xl-3 col-lg-4 col-sm-6">
-                 <div class="newproduct text-center" data-id="${data.id}">
-                    <div class="position-relative mb-3">
-                      <a class="d-block" href="detail.html">
-                         <img class="product-img" src="${data.image}" alt="..."></a>
-                       <div class="product-overlay">
-                         <ul class="mb-0 list-inline">
-                            <li class="list-inline-item m-0 p-0"><a class="btn " href="#"><i class="far fa-thumbs-up"></i></a></li>
-                            <li class="list-inline-item m-0 p-0"><a class="btn add-to-cart" href="#" ><i class="fas fa-cart-arrow-down"></i></a></li>
-                            <li class="list-inline-item m-0 p-0"><a class="btn " href="#"><i class="fas fa-info-circle"></i></a></li>
-                        </ul>
-                    </div>
-                     </div>
+
+
+ renderCart(){
+
+    this.clearCart.addEventListener('click',()=> this.clear());
+    this.cartItems.addEventListener('click', event=>{
+        if (event.target.classList.contains('fa-trash-alt')){
+            this.cart = this.filterItem(this.cart, event.target);
+            this.setCartTotal(this.cart);
         
-                  <h3> <a class="reset-anchor product-name" href="detail.html">${data.name}</a></h3>
-                   <p class="product-price " data-price="${data.price}">${data.price}</p>
-            </div>
-            </div>`             
+            this.cartItems.removeChild(event.target.parentElement.parentElement.parentElement);    
+        } else if(event.target.classList.contains('fa-caret-right')){
+             console.log(event.target.classList.contains('fa-caret-right'));
+            let tempItem = this.findItem(this.cart, event.target);
+            this.setCartTotal(this.cart);
+            tempItem.amount =  tempItem.amount + 1; 
+            event.target.previousElementSibling.innerHTML = tempItem.amount;
+          this.setCartTotal(this.cart);
+        
+    }
+        else if(event.target.classList.contains('fa-caret-left')){
+            let tempItem = this.findItem(this.cart, event.target);
+            tempItem.amount =  tempItem.amount - 1;
+            if (tempItem.amount>0){
+                
+            event.target.nextElementSibling.innerHTML = tempItem.amount;}
+            else {
+                this.cart = this.filterItem(this.cart, event.target);
+                this.cartItems.removeChild(event.target.parentElement.parentElement.parentElement);
+            };
+            this.setCartTotal(this.cart);
+            
+        };
+    });
 }
 
-function socIcon(className, icon, capture=''){
- return `<li class="list-inline-item m-0 p-0 ${className}"><a class="btn" href="#" ><i class="fas ${icon}"></i>${capture}</a></li>`;
+
 }
 
-function createNewMarkup(data){
-    return `
-    <div class="col-md-3 col-xl-3 col-lg-4 col-sm-6">
-                    <div class="newproduct text-center" data-id="${data.id}">
-                       <div class="position-relative mb-3">
-                         <a class="d-block" href="detail.html">
-                            <img class="product-img" src="${data.image}" alt="..."></a>
-                          <div class="product-overlay">
-                            <ul class="mb-0 list-inline">
-                               ${socIcon('like-this', 'fa-thumbs-up',)}
-                               ${socIcon('add-to-cart',' fa-cart-arrow-down', )}
-                               ${socIcon('view-this','fa-info-circle',)}
-                           </ul>
-                       </div>
-                        </div>     
-                     <h3> <a class="reset-anchor product-name" href="detail.html">${data.name}</a></h3>
-                      <p class="product-price " data-price="${data.price}">${data.price}</p>
-               </div>
-               </div>`             
-   }
-   function socIcon(className, icon, capture=''){
-    return `<li class="list-inline-item m-0 p-0 ${className}"><a class="btn" href="#" ><i class="fas ${icon}"></i>${capture}</a></li>`;
-   }
-   
-   function createNewMarkup(data){
-       return `
-       <div class="col-md-3 col-xl-3 col-lg-4 col-sm-6">
-                       <div class="newproduct text-center" data-id="${data.id}">
-                          <div class="position-relative mb-3">
-                            <a class="d-block" href="detail.html">
-                               <img class="product-img" src="${data.image}" alt="..."></a>
-                             <div class="product-overlay">
-                               <ul class="mb-0 list-inline">
-                                  ${socIcon('like-this', 'fa-thumbs-up',)}
-                                  ${socIcon('add-to-cart',' fa-cart-arrow-down', )}
-                                  ${socIcon('view-this','fa-info-circle',)}
-                              </ul>
-                          </div>
-                           </div>     
-                        <h3> <a class="reset-anchor product-name" href="detail.html">${data.name}</a></h3>
-                         <p class="product-price " data-price="${data.price}">${data.price}</p>
-                  </div>
-                  </div>`             
-      }
-   
-      function footerSocials(className, url, icon, capture=''){
+
+
+
+
+
+function footerSocials(className, url, icon, capture=''){
           return`
           <li><a class="footer-link ${className}" href="${url}" target="_blank"><i
           class="fab ${icon}"></i> ${capture}</a></li>`
-      }
-   
-      function footerService( url,  capture=''){
+}
+function footerService( url,  capture=''){
        return`
        <li><a class="footer-link" href="${url}">${capture} </a></li>`
-   }
-   function footerContact(className, url, icon, capture=''){
+}
+function footerContact(className, url, icon, capture=''){
        return`
        <li><a class="footer-link ${className}" href="${url}"><i class=" ${icon}"></i>${capture}</a></li>
        `
-   } 
-   function navbarNav(className, url, icon, capture=''){
+} 
+function navbarNav(className, url, icon, capture=''){
        return`
-   <li class="nav-item active ${className}"><a href="${url}" class="nav-link" target="_blank"><i
+   <li class="nav-item active ${className}"><a href="${url}" class="nav-link scroll-link" target="_blank"><i
    class="fas ${icon}"></i>${capture}</a></li>`
-   }
+}
+
+
+
 
 //=========
 (function(){
- toggleBtn.addEventListener('click', function () {
-  sidebar.classList.toggle('show-sidebar');
-  console.log(document.querySelectorAll('.cart-item').length);
-  
-  let itemInCart = document.querySelectorAll('.cart-item');
-  for (item of itemInCart){
-      console.log(item);
+    const app =new App();
      
-      const price = item.querySelector('.product-price').getAttribute('data-price');
-      item.querySelector('button.dec-btn').addEventListener('click', function(e){
-          console.log(e.target.parentNode.nextElementSibling);
-          let val = e.target.parentNode.nextElementSibling.textContent;
-          if (+(val)> 1){
-              val--;
-              console.log('val--:', val);
-              e.target.parentNode.nextElementSibling.textContent = val;
-                    e.target.closest('.quantity').nextElementSibling.querySelector('.product-price').textContent=val*price;
-          }
-      });
-      item.querySelector('button.inc-btn').addEventListener('click', function(e){
-          console.log(e.target.parentNode.previousElementSibling);
-          let val = e.target.parentNode.previousElementSibling.textContent;
-              +(val) ++;
-              console.log('val++:', val);
-              e.target.parentNode.previousElementSibling.textContent = val;
-              console.log(e.target.closest('.quantity').nextElementSibling.querySelector('.product-price').textContent);
-                e.target.closest('.quantity').nextElementSibling.querySelector('.product-price').textContent=val*price;
-              
-             
-          });
-      };
-      
-    });
-     closeBtn.addEventListener("click", function () {
-   sidebar.classList.remove("show-sidebar");
-    });
-     
-    let result = '';
-newproduct.forEach(function(item) {
-    // result+=createMarkup(item);
-    result+=createNewMarkup(item);
-});
 
 document.querySelector('.navbar-nav').innerHTML =
 `${navbarNav('', 'index.html', 'fa-home', 'Home')}
 ${navbarNav('','aboutUs.html','fa-book-open', 'About' )}
 ${navbarNav('','catalog.html', 'fa-blog','Catalog')}
 ${navbarNav('','SignUp.html', 'fa-address-card','Sign up')}`;
-
-document.querySelector('.footer-socials').innerHTML =
-`${footerSocials('twitter', 'https://twitter.com', 'fa-twitter', 'Twitter')}
-${footerSocials('facebook','https://www.facebook.com/','fa-facebook', 'Facebook' )}
-${footerSocials('instagram','https://www.instagram.com/', 'fa-instagram','Instagram')}
-${footerSocials('google','https://www.google.com/', 'fa-google-plus','Google')}`;
 
 
 document.querySelector('.footer-service').innerHTML =
@@ -172,7 +273,6 @@ ${footerService('#','Returns')}
 ${footerService('#','Refund')}
 ${footerService('#','Delivery')}`;
 
-
 document.querySelector('.footer-contact').innerHTML =
  `${footerContact('message','contactUS.html','fas fa-comment-dots','Leave a message')}
 ${footerContact('email','','fas fa-envelope','test@test.com')}
@@ -180,21 +280,8 @@ ${footerContact('phone','','fas fa-phone','+38 000 000 00 00')}
 ${footerContact('viber','','fab fa-viber', '+38 000 111 11 11')}
 ${footerContact('telegram','','fab fa-telegram', '+38 000 111 11 11')}`;
 
+app.addToCart();
+app.renderCart();
 
-document.querySelector('.showcase').innerHTML = result;
-     let content = document.getElementById("cartItem").content;
-        
-   const addToCart =document.getElementsByClassName("add-to-cart");
-const template =document.getElementById("cartItem").content;
-for(let i=0; i<addToCart.length; i++){
-    addToCart[i].addEventListener('click',function(e){
-        let item =e.target.closest('.newproduct');
-        let content =addProductToCart(template, item);
-        document.querySelector('.cart-items').append(document.importNode(content,true));
-let q = document.querySelector('.cart-toggle small').textContent;
-q++;
-document.querySelector('.cart-toggle small').textContent = q;
-})
-}
 })();
 
