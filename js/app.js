c67b6fbeefd2370726350f544909bb74d0ed0f02
SvetaSvitlychna <manulwild@gmail.com>
2021-02-06 17:00:40 +0200

    logo animation

---
+++ js/app.js
@@ -0,0 +1,382 @@
+'use strict';
+class Storage {
+    static saveProduct(newproduct) {
+        localStorage.setItem('newproduct', JSON.stringify(newproduct));
+    }
+    static getProduct(id) {
+        let newproduct = JSON.parse(localStorage.getItem('newproduct'));
+        return newproduct.find(product => product.id === +(id));
+    }
+    static getProducts() {
+        let newproduct = JSON.parse(localStorage.getItem('newproduct'));
+        return newproduct;
+    }
+    static saveCart(cart) {
+        localStorage.setItem("basket", JSON.stringify(cart));
+    }
+    static getCart() {
+        return localStorage.getItem("basket") ? JSON.parse(localStorage.getItem("basket")) : [];
+    }
+}
+
+class Product {
+    getProducts(newproduct) {
+        return newproduct.map(item => {
+            const name = item.name;
+            const price = item.price;
+            const id = item.id;
+            const image = item.image;
+            const category = item.category;
+            return { id, name, price, image, category };
+        });
+    }
+}
+const overlayGroup = [{
+        liClass: 'list-inline-item m-0 p-0 like-this',
+        aClass: 'btn',
+        icon: 'fas fa-thumbs-up',
+        urlClass: '',
+        capture: ''
+    },
+    {
+        liClass: 'list-inline-item m-0 p-0 add-to-cart',
+        aClass: 'btn',
+        icon: 'fas  fa-cart-arrow-down',
+        urlClass: '',
+        capture: ''
+    },
+    {
+        liClass: 'list-inline-item m-0 p-0 view-this',
+        aClass: 'btn',
+        icon: 'fas fa-info-circle',
+        capture: ''
+    },
+];
+const socialGroup = [{
+        liClass: 'footer-socials social-icon',
+        aClass: 'footer-link twitter',
+        icon: 'fab fa-twitter',
+        capture: 'Twitter'
+    },
+    {
+        liClass: 'footer-socials social-icon',
+        aClass: 'footer-link facebook',
+        icon: 'fab fa-facebook',
+        capture: 'Facebook'
+    },
+    {
+        liClass: 'footer-socials social-icon',
+        aClass: 'footer-link instagram',
+        icon: 'fab fa-instagram',
+        capture: 'Instagram'
+    },
+    {
+        liClass: 'footer-socials social-icon',
+        aClass: 'footer-link google',
+        icon: 'fab fa-google-plus',
+        capture: 'Google'
+    },
+];
+
+class App {
+    cart = [];
+    clearCart = document.querySelector(".clear-cart");
+    cartItems = document.querySelector(".cart-items");
+    sidebar = document.querySelector(".sidebar");
+    constructor() {
+        const toggleBtn = document.querySelector(".cart-toggle");
+        const closeBtn = document.querySelector(".close-btn");
+
+
+        toggleBtn.addEventListener('click', () => this.openCart());
+        closeBtn.addEventListener('click', () => this.closeCart());
+        this.navbarToggle();
+        document.querySelector('footer div.footerIcon').firstElementChild.innerHTML = this.makeLiGroup(socialGroup, 'navbar-footer footer-socials social-icon', '<h6 class="text-dark text-muted">Social media</h6>');
+        this.cart = Storage.getCart();
+    }
+
+    openCart() {
+        document.querySelector('.overlay').classList.add('active');
+        this.sidebar.classList.toggle("show-sidebar");
+        this.cartItems.innerHTML = '';
+        this.cart = Storage.getCart();
+        this.populateCart(this.cart);
+        // this.setItemTotal(this.cart);
+        this.setCartTotal(this.cart);
+    }
+    populateCart(cart) {
+        cart.forEach(item => this.addCartItem(item));
+    }
+    closeCart() {
+        this.sidebar.classList.remove("show-sidebar");
+        document.querySelector('.overlay').classList.remove('active');
+        this.cartItems.innerHTML = '';
+        this.cart = Storage.getCart();
+        this.populateCart(this.cart);
+        this.setCartTotal(this.cart);
+    }
+    navbarToggle() {
+        const navToggle = document.querySelector(".nav-toggle");
+        const linksContainer = document.querySelector(".linkscont");
+        const links = document.querySelector(".links-container");
+
+        navToggle.addEventListener('click', function() {
+            const linksHeight = links.getBoundingClientRect().height;
+            const containerHeight = linksContainer.getBoundingClientRect().height;
+            if (containerHeight === 0) {
+                linksContainer.style.height = `${linksHeight}px`;
+            } else {
+                linksContainer.style.height = 0;
+            }
+        });
+    }
+    makeLiGroup = (group, ulClass, header = '') => {
+        let lis = '';
+        group.forEach(function(item) {
+            lis += `<li class ="${item.liClass}">
+        <a class = "${item.aClass}" href="#">
+        <i class = "${item.icon}"></i>${item.capture}
+        </a>
+        </li>`;
+        });
+        return `
+    ${header}
+    <ul class ="${ulClass}">
+    ${lis}
+    </ul>`;
+    }
+
+    makeShowCase(newproduct) {
+        if (!document.querySelector('.showcase')) {
+            return;
+        }
+        let result = '';
+        newproduct.forEach(item => {
+            result += this.createProductMarkUp(item);
+        });
+        document.querySelector('.showcase').innerHTML = result;
+    }
+    createProductMarkUp(data) {
+        return `
+            <div  class="col-md-3 col-xl-3 col-lg-4 col-sm-6 cartItem">
+               <div class="newproduct text-center" data-id="${data.id}">
+                  <div class="position-relative mb-3">
+                    <a href="detail.html" class="d-block">
+                     <img src="${data.image}" alt="${data.name}" class="img-fluid w-100 product-img">
+                    </a>
+                     <div class="product-overlay">
+                     ${this.makeLiGroup(overlayGroup,'mb-0 list-inline')}
+                     </div>
+                   </div>
+                  <h3><a href="detail.html" class="reset-anchor product-name">${data.name}</a></h3>
+                 <p class="small text-muted product-price" data-price=${data.price}>${data.price}</p>
+                 </div>
+            </div> `;
+    }
+    getProduct = (id) => Storage.getProducts().find(product => product.id === +(id));
+
+    addToCart() {
+        const addToCartButtons = [...document.querySelectorAll(".add-to-cart")];
+        addToCartButtons.forEach(button => {
+            button.addEventListener('click', event => {
+                let product = this.getProduct(event.target.closest('.newproduct').getAttribute('data-id'));
+                let exist = this.cart.some(elem => elem.id === product.id);
+                if (exist) {
+                    this.cart.forEach(elem => {
+                        if (elem.id === product.id) {
+                            elem.amount += 1;
+                        }
+                    })
+                } else {
+                    let cartItem = {...product, amount: 1 };
+                    this.cart = [...this.cart, cartItem];
+                }
+                // this.addCartItem(cartItem);
+                // this.setItemTotal(this.cart);
+                this.setCartTotal(this.cart);
+                Storage.saveCart(this.cart);
+            });
+        });
+    }
+    addCartItem(item) {
+        const div = document.createElement("div");
+        div.classList.add("cart-item");
+        div.setAttribute('id', item.id);
+        div.innerHTML = `
+            <div class="picture product-img">
+                    <img src="${item.image}" alt="${item.name}" class="img-fluid w-100">
+                </div>
+                <div class="product-name p-auto">${item.name}</div>
+                <div class="remove-btn text-right">
+                    <a class="reset-anchor m-auto" href="#">
+                        <i class="fas fa-trash-alt" data-id=${item.id}></i>
+                    </a>
+                </div>
+                <div class="quantity">
+                    <div class="border d-flex justify-content-around mx-auto">
+                        <i class="fas fa-caret-left inc-dec-btn" data-id=${item.id}></i>
+                        <span class="border-1 p-1 amount">${item.amount}</span>
+                        <i class="fas fa-caret-right inc-dec-btn" data-id=${item.id}></i>
+                    </div>
+                </div>
+                <div class="price">
+                    $<span class="product-price">${item.price}</span>
+                </div>`;
+        this.cartItems.appendChild(div);
+    }
+    filterItem = (cart, curentItem) => cart.filter(item => item.id !== +(curentItem.dataset.id));
+
+    findItem = (cart, curentItem) => cart.find(item => item.id === +(curentItem.dataset.id));
+
+    clear = () => {
+        this.cart = [];
+        while (this.cartItems.children.length > 0) {
+            this.cartItems.removeChild(this.cartItems.children[0]);
+        }
+        // this.setItemTotal(this.cart);
+        this.setCartTotal(this.cart);
+        Storage.saveCart(this.cart);
+
+    }
+
+    // setItemTotal(cart) {
+
+    //     document.querySelector('.product-price').textContent = parseFloat(cart.reduce((p, c) => p + c.amount * c.price, 0).toFixed(2));
+    //     document.querySelector('.count-items').textContent = cart.reduce((prev, cur) => prev + cur.amount, 0);
+    // }
+    setCartTotal(cart) {
+
+        document.querySelector('.cart-total').textContent = parseFloat(cart.reduce((p, c) => p + c.amount * c.price, 0).toFixed(2));
+        document.querySelector('.count-items').textContent = cart.reduce((prev, cur) => prev + cur.amount, 0);
+    }
+
+    renderCart() {
+
+        this.clearCart.addEventListener('click', () => this.clear());
+        this.cartItems.addEventListener('click', event => {
+            if (event.target.classList.contains('fa-trash-alt')) {
+                this.cart = this.filterItem(this.cart, event.target);
+                // this.setItemTotal(this.cart);
+                this.setCartTotal(this.cart);
+                Storage.saveCart(this.cart);
+                this.cartItems.removeChild(event.target.parentElement.parentElement.parentElement);
+            } else if (event.target.classList.contains('fa-caret-right')) {
+                console.log(event.target.classList.contains('fa-caret-right'));
+                let tempItem = this.findItem(this.cart, event.target);
+                // this.setItemTotal(this.cart);
+                this.setCartTotal(this.cart);
+                Storage.saveCart(this.cart);
+                tempItem.amount = tempItem.amount + 1;
+
+                event.target.previousElementSibling.innerHTML = tempItem.amount;
+                // this.setItemTotal(this.cart);
+                this.setCartTotal(this.cart);
+                Storage.saveCart(this.cart);
+            } else if (event.target.classList.contains('fa-caret-left')) {
+                let tempItem = this.findItem(this.cart, event.target);
+                tempItem.amount = tempItem.amount - 1;
+                if (tempItem.amount > 0) {
+
+                    event.target.nextElementSibling.innerHTML = tempItem.amount;
+                } else {
+                    this.cart = this.filterItem(this.cart, event.target);
+                    this.cartItems.removeChild(event.target.parentElement.parentElement.parentElement);
+                };
+                // this.setItemTotal(this.cart);
+                this.setCartTotal(this.cart);
+                Storage.saveCart(this.cart);
+            };
+        });
+    }
+
+    renderCategory() {
+        const categories = document.querySelector('.categories');
+        if (categories) {
+            categories.addEventListener('click', event => {
+                event.preventDefault();
+                const target = event.target;
+                if (target.classList.contains('category-item')) {
+                    const category = target.dataset.category;
+                    // console.log(category);
+                    const categoryFilter = items => items.filter(item => item.category.includes(category));
+                    //   console.log(categoryFilter);
+                    this.makeShowCase(categoryFilter(Storage.getProducts()));
+
+                } else {
+                    this.makeShowCase(Storage.getProducts());
+                }
+                this.addToCart();
+                this.renderCart();
+            });
+        }
+    }
+
+}
+
+function footerSocials(className, url, icon, capture = '') {
+    return `
+          <li><a class="footer-link ${className}" href="${url}" target="_blank"><i
+          class="fab ${icon}"></i> ${capture}</a></li>`
+}
+
+function footerService(url, capture = '') {
+    return `
+       <li><a class="footer-link" href="${url}">${capture} </a></li>`
+}
+
+function footerContact(className, url, icon, capture = '') {
+    return `
+       <li><a class="footer-link ${className}" href="${url}"><i class=" ${icon}"></i>${capture}</a></li>
+       `
+}
+
+function navbarNav(className, url, icon, capture = '') {
+    return `
+   <li class="nav-item active ${className}"><a href="${url}" class="nav-link scroll-link" target="_blank"><i
+   class="fas ${icon}"></i>${capture}</a></li>`
+}
+
+
+
+(function() {
+    const url = 'https://my-json-server.typicode.com/SvetaSvitlychna/dbsecond/data';
+    fetch(url)
+        .then(response => {
+            response.json()
+                .then(newproduct => {
+                    const app = new App();
+                    let data = new Product();
+                    Storage.saveProduct(data.getProducts(newproduct));
+                    console.log(data.getProducts(newproduct))
+                    app.makeShowCase(newproduct);
+                    app.addToCart();
+                    app.renderCart();
+                    app.renderCategory();
+                })
+        })
+        .catch(err => {
+            console.log(err);
+        })
+
+    document.querySelector('.navbar-nav').innerHTML =
+        `${navbarNav('', 'index.html', 'fa-home', 'Home')}
+${navbarNav('','aboutUs.html','fa-book-open', 'About' )}
+${navbarNav('','catalog.html', 'fa-blog','Catalog')}
+${navbarNav('','SignUp.html', 'fa-address-card','Sign up')}`;
+
+
+    document.querySelector('.footer-service').innerHTML =
+        `${footerService('#','Help')}
+${footerService('#','FAQs')}
+${footerService('#','Returns')}
+${footerService('#','Refund')}
+${footerService('#','Delivery')}`;
+
+    document.querySelector('.footer-contact').innerHTML =
+        `${footerContact('message','contactUS.html','fas fa-comment-dots','Leave a message')}
+${footerContact('email','','fas fa-envelope','test@test.com')}
+${footerContact('phone','','fas fa-phone','+38 000 000 00 00')}
+${footerContact('viber','','fab fa-viber', '+38 000 111 11 11')}
+${footerContact('telegram','','fab fa-telegram', '+38 000 111 11 11')}`;
+
+})();
---
+++ js/data.js
@@ -0,0 +1,227 @@
+const newproduct =[
+    {id:1,
+        image:'images/prod1W.jpg',
+        name:'Bicycle 29 ',
+        price: 320,
+        category:'bicycle'
+    },
+{id:2,
+        image:'images/prod2M.jpg',
+        name:'Bicycle 16',
+        price: 310,
+        category:'bicycle'
+},
+{id:3,
+        image:'images/prod3M.jpg',
+        name:'Bicycle 28',
+        price: 300,
+        category:'bicycle'
+},
+{id:4,
+            image:'images/prod4M.jpg',
+            name:'Bicycle 14',
+            price: 170,
+            category:'bicycle'
+        },
+{id:5,
+        image:'images/prod5K.jpg.png',
+        name:'Bicycle 26',
+        price: 200,
+        category:'bicycle'
+},
+{id:6,
+        image:'images/prod6K.jpg',
+        name:'Bicycle 25',
+        price: 210,
+        category:'bicycle'
+},
+{id:7,
+        image:'images/Prod8T.jpg',
+        name:'Bicycle 24',
+        price:240 ,
+        category:'bicycle'
+},
+{id:8,
+        image:'images/prod9K.jpg',
+        name:'Bicycle 23',
+        price:270 ,
+        category:'bicycle'
+},
+{id:9,
+        image:'images/prod10E.jpg',
+        name:'Bicycle 27',
+        price: 300,
+        category:'bicycle'
+},
+{id:10,
+        image:'images/Prod11U.jpg',
+        name:'Bicycle 22',
+        price:900 ,
+        category:'bicycle'
+},
+{id:11,
+image:'images/prod12M.jpg',
+name:'Bicycle 21',
+price:250 ,
+category:'bicycle'
+},
+{id:12,
+image:'images/prod13T.jpg',
+name:'Bicycle 20',
+price:150 ,
+category:'bicycle'
+},
+{id:13,
+image:'images/prod14U.jpg',
+name:'Bicycle 19',
+price:100 ,
+category:'bicycle'
+},
+{id:14,
+image:'images/prod15W.png',
+name:'Bicycle 18',
+price:240 ,
+category:'bicycle'
+},
+{id:15,
+image:'images/prod17W.jpg',
+name:'Bicycle 2',
+price:200 ,
+category:'bicycle'
+},
+{id:16,
+image:'images/prod18M.jpg',
+name:'Bicycle 3',
+price: 2700,
+category:'bicycle'
+},
+{id:17,
+image:'images/prod19K.jpg',
+name:'Bicycle 4',
+price: 500,
+category:'bicycle'
+},
+{id:18,
+image:'images/prod20AS.jpg',
+name:'Accessories 1',
+price: 460,
+category:'accessories'
+},
+{id:19,
+image:'images/prod21AS.jpg',
+name:'Accessories 2',
+price:470 ,
+category: 'accessories'
+},
+{id:20,
+image:'images/prod21K.jpg',
+name:'Bicycle 5',
+price: 490,
+category:'bicycle'
+},
+{id:21,
+image:'images/prod22AS.jpg',
+name:'Accessories 5',
+price:480 ,
+category:'accessories'
+},
+
+{id:23,
+image:'images/prod24AS.jpg',
+name:'Assessories 7',
+price:460 ,
+categoty:'accessories'
+},
+{id:22,
+image:'images/prod23AS.jpeg',
+name:'Assessories 6',
+price: 470,
+categoty:'accessories'
+},
+{id:36,
+image:'images/prod25K.jpg',
+name:'Bicycle 7',
+price:430,
+categoty:'bicycle'
+},
+{id:24,
+image:'images/prod26U.jpg',
+name:'Bicycle 8',
+price:420,
+categoty:'bicycle'
+},
+{id:25,
+image:'images/prod27UT.jpg',
+name:'Bicycle 9',
+price:410,
+categoty:'bicycle'
+},
+{id:26,
+image:'images/prod28U.jpg',
+name:'Bicycle 10',
+price:400,
+category:'bicycle'
+},
+{id:27,
+image:'images/prod29U.jpg',
+name:'Bicycle 11',
+price:207,
+category:'bicycle'
+},
+{id:28,
+image:'images/Prod30W.jpg',
+name:'Bicycle 12',
+price:550,
+category:'bicycle'
+},
+{id:29,
+image:'images/prod31W.jpg',
+name:'Bicycle 13',
+price:360,
+category:'bicycle'
+},
+{id:30,
+image:'images/prod33ClothM.jpg',
+name:'Clothes 1',
+price:350,
+category:'clothes'
+},
+{id:31,
+image:'images/prod34UT.jpg',
+name:'Bicucle 14',
+price:340,
+category:'bicycle'
+},
+{id:32,
+image:'images/prod35UT.jpeg',
+name:'Bicycle 15',
+price:280,
+category:'bicycle'
+},
+{id:33,
+image:'images/prod36UT.jpg.png',
+name:'Bicycle 17',
+price:260,
+category:'bicycle'
+},
+{id:34,
+image:'images/prodCloth32M.jpg',
+name:'Clothes 2',
+price:290,
+category:'clothes'
+},
+{id:35,
+image:'images/top7.jpg',
+name:'Bicycle 1',
+price:330,
+category:'bicycle'
+}
+];
+// newproduct.forEach(function(item){
+// for (let key  in item)
+// {    console.log(key+': '+item [key]);
+// }
+// });
+
+
+
