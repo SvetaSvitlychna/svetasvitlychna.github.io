2dec591ef47b83806ff0e788c13113df045dcf12
SvetaSvitlychna <manulwild@gmail.com>
2021-02-07 17:08:03 +0200

    col-3 was added for product list

--- js/app.js
+++ js/app.js
@@ -158,7 +158,7 @@ class App {
     }
     createProductMarkUp(data) {
         return `
-            <div  class="col-md-3 col-xl-3 col-lg-4 col-sm-6 cartItem">
+            <div  class="col-4 col-md-3 col-xl-3 col-lg-4  ">
                <div class="newproduct text-center" data-id="${data.id}">
                   <div class="position-relative mb-3">
                     <a href="detail.html" class="d-block">
